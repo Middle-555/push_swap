9999ba9b990365079f0b3fbab7726341efd85358
Middle <pourcel.kevin@gmail.com>
2024-04-08 19:11:18 +0200

    init

---
+++ include/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 16:07:38 by kpourcel          #+#    #+#             */
+/*   Updated: 2024/04/08 19:10:23 by kpourcel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <limits.h>
+
+
+# endif
