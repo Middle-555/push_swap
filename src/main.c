a4a70405d25feeeb1022a710d524bbcec5b186f0
Middle <pourcel.kevin@gmail.com>
2024-04-10 23:42:35 +0200

    test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:07:16 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/04/10 16:10:32 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/04/10 23:42:20 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int	main(int argc, char **argv)
 {
+	char	**tab;
+
 	if (argc == 2)
-		error_msg("./push_swap [array of int]");
+		tab = ft_split(argv[1], 32);
 	else if (argc > 1)
 		error_msg("./push_swap [array of int]");
 	else
