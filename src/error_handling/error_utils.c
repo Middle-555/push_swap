eb2982dbbd63d8714c8c11b7f1591721031f0023
Middle <pourcel.kevin@gmail.com>
2024-04-12 16:56:16 +0200

    correction error in check_number

--- src/error_handling/error_utils.c
+++ src/error_handling/error_utils.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:00:45 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/04/12 16:49:14 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/04/12 16:55:50 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	check_number(char **str)
 			else
 				error_msg("You can only put number in the array !");
 		}
+		mot++;
 	}
 }
 
