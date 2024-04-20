f8abbc9f7b7116e78fba30ffd3a40dd2b761b861
Middle <pourcel.kevin@gmail.com>
2024-04-20 16:51:16 +0200

    need to do 3entries

--- src/sorting/3_entries.c
+++ src/sorting/3_entries.c
@@ -6,8 +6,17 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:28:16 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/04/20 13:01:11 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/04/20 16:05:54 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
+
+void	sort_3_entries(t_stack **stack)
+{
+	t_stack	*lowest;
+	t_stack	*highest;
+
+	lowest = find_lowest_node(stack);
+	highest = find_highest_node(stack);
+}
