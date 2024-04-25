aa3b7959641ad22f5a165f9a5792eaf378c566a6
Middle <pourcel.kevin@gmail.com>
2024-04-25 14:55:19 +0200

    fixed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:07:16 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/04/25 14:41:42 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/04/25 14:55:01 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ int	main(int argc, char **argv)
 		all_error_checking(tab);
 		init_stack(&data);
 		fill_stack_a(tab, &data);
-		if (!(check_lst_sorted(&data.stack_a)))
+		if (check_lst_sorted(&data.stack_a) == 1)
 		{
 			entries_handler(&data.stack_a);
 		}
-		print_tab(tab);
+		lst_print(data.stack_a);
 		free_both_stack(&data);
 	}
 }
