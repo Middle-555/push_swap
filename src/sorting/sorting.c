5a05de2d4ed451b217fa7f90fa148cc6f4126e3d
Middle <pourcel.kevin@gmail.com>
2024-05-03 12:00:24 +0200

    need rework push_b to a

--- src/sorting/sorting.c
+++ src/sorting/sorting.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 15:38:25 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/05/02 23:40:38 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/05/03 11:59:29 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,13 @@ void	entries_handler(t_stack **stack_a, t_stack **stack_b)
 
 	(void)stack_b;
 	stack_size_a = stack_size(*stack_a);
-	ft_printf(" Stack size : %d\n", stack_size_a);
 	if (stack_size_a == 2)
-	{
-		print_stacks(*stack_a, *stack_b);
 		sort_2_entries(stack_a);
-		print_stacks(*stack_a, *stack_b);
-	}
 	else if (stack_size_a == 3)
-	{
-		print_stacks(*stack_a, *stack_b);
 		sort_3_entries(stack_a);
-		print_stacks(*stack_a, *stack_b);
-	}
 	else if (stack_size_a == 4)
 	{
-		push_to_stack_b(stack_a, stack_b);       
+		push_to_stack_b(stack_a, stack_b);      
 		sort_3_entries(stack_a);
 	}
 	else if (stack_size_a >= 5)
@@ -52,13 +43,11 @@ void	entries_handler(t_stack **stack_a, t_stack **stack_b)
 			sorting(stack_a, stack_b);
 			push_to_stack_b(stack_a, stack_b);
 			stack_size_a = stack_size(*stack_a);
-			print_stacks(*stack_a, *stack_b);
 		}
 		sort_3_entries(stack_a);
 		while (stack_size(*stack_b) > 0)
 			push_to_stack_a(stack_a, stack_b);
 		while (*stack_a != find_lowest_node(stack_a))
 			rotate_stack_a(stack_a);
-		print_stacks(*stack_a, *stack_b);
 	}
 }
