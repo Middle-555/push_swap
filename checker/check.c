1334fbe400e6daf2391d6c634b8c5438eb179ce1
Middle <pourcel.kevin@gmail.com>
2024-05-06 19:24:58 +0200

    ok avec l'autre gnl

--- checker/check.c
+++ checker/check.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:58:01 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/05/06 18:31:14 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/05/06 19:02:15 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,45 +25,32 @@ int	get_instruction(t_stack **stack_a, t_stack **stack_b)
 	char	*instruction;
 
 	instruction = get_next_line(0);
-	printf("0\n");
 	while (instruction && instruction[0] != '\n')
 	{
-		printf("printavant le if \n");
-		if (instruction_check(stack_a, stack_b, instruction))
+		if (!instruction_check(stack_a, stack_b, instruction))
 		{
-			printf("1\n");
 			free(instruction);
-			printf("2\n");
 			ft_printf("Error\n");
-			printf("3\n");
 			return (-1);
 		}
-		printf("print entre le if \n");
-		if (check_lst_sorted(stack_a) == (-1) && stack_b == NULL)
+		if (check_lst_sorted(stack_a) == 0 && stack_b == NULL)
 		{
-			printf("4\n");
 			free(instruction);
-			printf("5\n");
 			return (0);
 		}
-		printf("6\n");
 		free(instruction);
-		printf("7\n");
 		instruction = get_next_line(0);
-		printf("8\n");
 	}
-	printf("9\n");
 	free(instruction);
-	printf("10\n");
 	return (0);
 }
 
 int	instruction_check(t_stack **stack_a, t_stack **stack_b, char *str)
 {
 	if (ft_strcmp(str, "pa\n") == 0)
-		push(stack_a, stack_b);
-	else if (ft_strcmp(str, "pb\n") == 0)
 		push(stack_b, stack_a);
+	else if (ft_strcmp(str, "pb\n") == 0)
+		push(stack_a, stack_b);
 	else if (ft_strcmp(str, "sa\n") == 0)
 		swap(stack_a);
 	else if (ft_strcmp(str, "sb\n") == 0)
@@ -73,9 +60,9 @@ int	instruction_check(t_stack **stack_a, t_stack **stack_b, char *str)
 		swap(stack_a);
 		swap(stack_b);
 	}
-	else if (instruction_check_rotate(stack_a, stack_b, str) == -1)
-		return (-1);
-	return (0);
+	else if (instruction_check_rotate(stack_a, stack_b, str) == 0)
+		return (0);
+	return (1);
 }
 
 int	instruction_check_rotate(t_stack **stack_a, t_stack **stack_b, char *str)
@@ -99,6 +86,6 @@ int	instruction_check_rotate(t_stack **stack_a, t_stack **stack_b, char *str)
 		reverse_rotate(stack_b);
 	}
 	else
-		return (-1);
-	return (0);
+		return (0);
+	return (1);
 }
--- checker/checker.c
+++ checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:58:14 by kpourcel          #+#    #+#             */
-/*   Updated: 2024/05/06 18:31:05 by kpourcel         ###   ########.fr       */
+/*   Updated: 2024/05/06 18:57:00 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,12 @@ int	main(int argc, char **argv)
 		error_msg("Usage: ./push_swap [tableau d'entiers]");
 		return (1);
 	}
-	printf("main 1\n");
 	tab = init_tab(argc, argv);
-	printf("main 2\n");
 	all_error_checking(tab);
-	printf("main 3\n");
 	init_stack(&data);
-	printf("main 4\n");
 	fill_stack_a(tab, &data);
-	printf("main 5\n");
 	if (check_lst_sorted(&data.stack_a) == 0)
 		free_both_stack(&data);
-	printf("main 6\n");
 	else
 	{
 		get_instruction(&data.stack_a, &data.stack_b);
